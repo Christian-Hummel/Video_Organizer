967f81ef050dcdd434efe93b90c598406ae77055
Christian-Hummel <chrishummel@gmx.at>
2026-02-18 18:54:59 +0100

    added function to store new movie in database

--- video_organizer/views.py
+++ video_organizer/views.py
@@ -1,6 +1,10 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+from .models import Movie
 
 
 import requests
@@ -22,3 +26,27 @@ def overview(request):
 def rooms(request):
 
     return render(request, "rooms.html")
+
+def dvd(request):
+
+    return render(request, "dvd.html")
+
+def process_movie_entry(request):
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        year = request.POST.get('year')
+        description = request.POST.get('description')
+        director = request.POST.get('director')
+        type = request.POST.get('type')
+        picture_description = request.POST.get('picture_description')
+        picture = request.FILES.get('picture')
+
+        movie = Movie(title=title, year=year, description=description, director=director,type=type, picture_description=picture_description,picture=picture)
+
+        movie.save()
+    
+
+        return render(request, "movie_success.html")
+    else:
+        return HttpResponse("Invalid request method!")
