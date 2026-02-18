3278d92e5533ff5d7817e3dfa929c1bfd4947a69
Christian-Hummel <chrishummel@gmx.at>
2026-02-18 21:15:13 +0100

    added bootsrap configuration for messages

--- DVD_Organizer/settings.py
+++ DVD_Organizer/settings.py
@@ -127,3 +127,13 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
